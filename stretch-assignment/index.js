757b89840ea62605d4fafd3222c082eeff27a0a4
Alice Chang <sinsofangels@gmail.com>
2020-09-29 19:21:08 -0400

    got rockets working

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,27 +1,60 @@
 let blocks = document.querySelectorAll(".block");
 
-for (let i in blocks) {
-    blocks[i].addEventListener("click", jump);
+//add event listeners
+window.addEventListener("mouseup", mouseUp); //can't set event listener to block because block moves. mouseup won't log unless it's on the block. must set it in window.
+window.addEventListener("mousedown", blastOff);
+for (let i of blocks) {
+    i.addEventListener("click", jump); //single click
+    // i.addEventListener("mousedown", blastOff);
 }
 
+let mouseDown = false;
+let currentPos = 10;
+let blast;
+let movedBlock;
+
+//event functions
 function jump(e) {
     let offset = 110;
-    let chosen;
+    mouseDown = false;
     for (let i in blocks) {
         if (blocks[i] === e.target && i > 0) {
-            offset = 120*i;
-            chosen = blocks[i];
+            offset = 120*i; //each block is 100 px plus margin. i is number of blocks to jump
+            movedBlock = blocks[i];
         }
     }
-    let counter = 1;
+    let counter = 1; // 
     for (let i of blocks) {
-        if (i === chosen) {
-            chosen.style.order = 0;
+        if (i === movedBlock) {
+            movedBlock.style.order = 0;
         }
         else {
             i.style.order = counter;
             counter++;
         }
     }
-    gsap.fromTo(e.target, {y: `${offset}`}, {duration: 1, y: 0});
+    gsap.fromTo(movedBlock, {y: `${offset}`}, {duration: 1, y: 0});
+    movedBlock = null;
+}
+
+function blastOff(e) {
+    mouseDown = true;
+    setTimeout(() => {
+        if (mouseDown) {
+            blast = setInterval(move, 100);
+            if (!movedBlock) movedBlock = e.target;
+            function move() {
+                if (currentPos < 900) {
+                    gsap.to(movedBlock, {duration: 1, x:`${currentPos+100}`});
+                    currentPos = currentPos+100;
+                }
+            }
+        }
+    }, 150);
+}   
+
+function mouseUp() {
+    clearInterval(blast);
+    gsap.to(movedBlock, {duration: 1, x: 0});
+    currentPos = 0;
 }
